c3c5402c72b6a494eb59f357f632ef057c9f1188
Leandro <leandro.chavez@virginiogomez.cl>
2021-12-14 16:28:02 -0300

    adelanto

--- app/src/main/java/com/example/certamen2leandro/MainActivity.java
+++ app/src/main/java/com/example/certamen2leandro/MainActivity.java
@@ -2,18 +2,29 @@ package com.example.certamen2leandro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button siguiente;
+    Button btnventana2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        siguiente=(Button) findViewById(R.id.ventana2);
+
+        btnventana2=(Button) findViewById(R.id.btnventana2);
+
+        btnventana2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent btnventana2 = new Intent(MainActivity.this, ventana2.class );
+                startActivity(btnventana2);
+            }
+        });
 
     }
 }
