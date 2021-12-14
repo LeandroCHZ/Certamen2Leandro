c12d9f769c43d781abe893013b68f76c510f260a
Leandro <leandro.chavez@virginiogomez.cl>
2021-12-14 18:58:53 -0300

    A MEDIAS

--- app/src/main/java/com/example/certamen2leandro/ventana2.java
+++ app/src/main/java/com/example/certamen2leandro/ventana2.java
@@ -1,14 +1,45 @@
 package com.example.certamen2leandro;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ventana2 extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class ventana2 extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ventana2);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng Yungay = new LatLng(-37.049102, -72.1828642);
+        mMap.addMarker(new MarkerOptions()
+                .position(Yungay)
+                .title("Salto del Itata Yungay"));
+        mMap.setMapType(googleMap.MAP_TYPE_SATELLITE);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Yungay,30));
+
     }
 }
