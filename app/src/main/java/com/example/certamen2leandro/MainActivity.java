5815f485e84a5d14a2b8c10cde41dd643b930ec3
Leandro <leandro.chavez@virginiogomez.cl>
2021-12-15 12:42:58 -0300

    casi

--- app/src/main/java/com/example/certamen2leandro/MainActivity.java
+++ app/src/main/java/com/example/certamen2leandro/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.certamen2leandro;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity<edad, nombre> extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    private EditText nombre;
+    private EditText edad;
+    private EditText sexo;
+
 
 
 
@@ -22,12 +39,55 @@ public class MainActivity extends AppCompatActivity {
         btnventana2=(Button) findViewById(R.id.btnventana2);
 
         btnventana2.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("WrongViewCast")
             @Override
             public void onClick(View view) {
                 Intent btnventana2 = new Intent(MainActivity.this, ventana2.class );
                 startActivity(btnventana2);
+
+                mAuth = FirebaseAuth.getInstance();
+
+                nombre = findViewById(R.id.btnventana2);
+                 edad = findViewById(R.id.btnventana2);
+
+
+
             }
         });
 
     }
+
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+    public void iniciar(View view) {
+
+    }
+
+    mAuth.FirebaseAuth.AuthStateListener(nombre, edad)
+
+    void addOnCompleteListener(new, this OnCompleteListener<AuthResult>() {
+
+    }
+        @Override
+        public void onComplete(@NonNull Task<AuthResult> task) {
+            if (task.isSuccessful()) {
+
+                FirebaseUser user = mAuth.getCurrentUser();
+
+            } else {
+
+
+                Toast.makeText(this, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show();
+                //updateUI(null);
+            }
+
+
+        }
+
 }
