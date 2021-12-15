9d0cb6d2e3616cfbdd9098a17f539d1c9fc50ede
Leandro <leandro.chavez@virginiogomez.cl>
2021-12-15 12:58:19 -0300

    casi listo

--- app/src/main/java/com/example/certamen2leandro/MainActivity.java
+++ app/src/main/java/com/example/certamen2leandro/MainActivity.java
@@ -6,11 +6,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
+import android.util.Log;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
